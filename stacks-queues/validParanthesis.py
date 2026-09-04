a29517d0c8525da0dd50a23d56f2cf8a1824ee92
24f3003058 <24f3003058@ds.study.iitm.ac.in>
2026-09-04 12:01:17 +0530

    valid paranthesis problem

---
+++ stacks-queues/validParanthesis.py
@@ -0,0 +1,14 @@
+def is_valid(s):
+    # Write your code here
+    pass
+    stack=[]
+    pairs ={ ')':'(',  '}':'{',  ']':'['}
+    for char in s:
+        if char in pairs:
+            if not stack or stack.pop() != pairs[char]:
+                return False
+                
+            else:
+                stack.append(char)
+                
+    return not stack
